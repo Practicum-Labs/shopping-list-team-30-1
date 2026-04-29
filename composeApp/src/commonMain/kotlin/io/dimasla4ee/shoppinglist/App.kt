d6456101570d6c9869280dfafbb00175fe92eb48
dekry <de.krylov@vk.com>
2026-04-29 11:32:48 +0300

    feat: Обновить App.kt для проверки изображений

--- composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/App.kt
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/App.kt
@@ -7,23 +7,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
-
 import shoppinglist.composeapp.generated.resources.Res
-import shoppinglist.composeapp.generated.resources.compose_multiplatform
+import shoppinglist.composeapp.generated.resources.ic_add_circle_24
+import shoppinglist.composeapp.generated.resources.ic_remove_circle_24
+import shoppinglist.composeapp.generated.resources.img_main_screen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        val iconRes = remember(showContent) {
+            if (showContent) Res.drawable.ic_add_circle_24 else Res.drawable.ic_remove_circle_24
+        }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -31,8 +40,13 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            IconButton(
+                onClick = { showContent = !showContent }
+            ) {
+                Icon(
+                    painter = painterResource(iconRes),
+                    contentDescription = null
+                )
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
@@ -40,7 +54,10 @@ fun App() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Image(
+                        painter = painterResource(Res.drawable.img_main_screen),
+                        contentDescription = null
+                    )
                     Text("Compose: $greeting")
                 }
             }
