4249a50933d6888c999e75e2dfbf92a80396cbd3
Krylov Dmitry Evgenyevich <de.krylov@vk.com>
2026-05-02 22:18:59 +0300

    feat: Реализовать доменную модель для списка покупок
    - Реализовал доменную модель для списка покупок ShoppingList.kt
    - Реализовал доменный enum-класс с иконками ShoppingListIcon.kt
    - Реализовал маппер для конвертации ShoppingListIcon в DrawableResource
    - Реализовал юнит тест, проверяющий корректность работы маппера

---
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/domain/model/ShoppingList.kt
@@ -0,0 +1,8 @@
+package io.dimasla4ee.shoppinglist.core.domain.model
+
+data class ShoppingList(
+    val id: Long,
+    val name: String,
+    val icon: ShoppingListIcon,
+    val products: List<ShoppingList>
+)
