4abc7f00ed83c3479ac9764a9909bad8fa64a529
Krylov Dmitry Evgenyevich <de.krylov@vk.com>
2026-05-02 23:39:38 +0300

    feat(db): Реализовать сущности базы данных товар и список покупок

---
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/database/entity/ProductEntity.kt
@@ -0,0 +1,25 @@
+package io.dimasla4ee.shoppinglist.core.database.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import io.dimasla4ee.shoppinglist.core.domain.model.MeasurementUnit
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = ShoppingListEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["listId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ProductEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val listId: Long,
+    val name: String,
+    val amount: Float,
+    val unit: MeasurementUnit,
+    val addedAtMillis: Long = System.currentTimeMillis()
+)
