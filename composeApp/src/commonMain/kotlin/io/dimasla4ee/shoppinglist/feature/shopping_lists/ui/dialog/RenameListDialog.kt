5954590c38167e6fbf33aa0f7a68edebc1c071ed
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-05-10 00:28:02 +0500

    depends-on-PR#85: создание диалогов для удаления одного списка, и диалога для переименования

---
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/feature/shopping_lists/ui/dialog/RenameListDialog.kt
@@ -0,0 +1,124 @@
+package io.dimasla4ee.shoppinglist.feature.shopping_lists.ui.dialog
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import io.dimasla4ee.shoppinglist.app.ui.theme.AppDimensions
+import io.dimasla4ee.shoppinglist.app.ui.theme.AppTypography
+import io.dimasla4ee.shoppinglist.app.ui.theme.ShoppingListTheme
+import io.dimasla4ee.shoppinglist.app.ui.theme.dialogTextFieldColors
+import org.jetbrains.compose.resources.stringResource
+import shoppinglist.composeapp.generated.resources.Res
+import shoppinglist.composeapp.generated.resources.button_cancel
+import shoppinglist.composeapp.generated.resources.button_rename
+import shoppinglist.composeapp.generated.resources.dialog_rename_list
+import shoppinglist.composeapp.generated.resources.hint_name_list
+
+@Composable
+fun RenameListDialog(
+    newName: String,
+    onRenameChange: (String) -> Unit,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+
+        // фон диалога
+        containerColor = MaterialTheme.colorScheme.tertiary,
+
+        // скругление углов
+        shape = RoundedCornerShape(AppDimensions.DialogAddition.cornerRadius),
+
+        title = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(AppDimensions.DialogAddition.spaceIconTextField)
+            ) {
+                Text(
+                    text = stringResource(Res.string.dialog_rename_list),
+                    style = AppTypography.titleLarge
+                )
+            }
+        },
+
+        text = {
+            OutlinedTextField(
+                value = newName,
+                onValueChange = onRenameChange,
+                singleLine = true,
+
+                label = {
+                    Text(
+                        text = stringResource(Res.string.hint_name_list),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                },
+
+                colors = dialogTextFieldColors()
+            )
+        },
+
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm,
+                enabled = newName.isNotBlank(),
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiary
+                )
+            ) {
+                Text(
+                    text = stringResource(Res.string.button_rename),
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+        },
+
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss,
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiary
+                )
+            ) {
+                Text(
+                    text = stringResource(Res.string.button_cancel),
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+        },
+    )
+}
+
+@Preview
+@PreviewLightDark
+@Composable
+private fun CreateListDialogAllPreview() {
+    var text by remember { mutableStateOf("") }
+
+    ShoppingListTheme {
+        RenameListDialog(
+            newName = text,
+            onRenameChange = { text = it },
+            onDismiss = {},
+            onConfirm = {}
+        )
+    }
+}
