dfb6dfa68520f1e746e3709834dd764c2e824503
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-05-10 00:29:50 +0500

    depends-on-PR#85: классы и интерфейсы для слоев data, domain для фичи Списки продуктов

---
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/feature/shopping_lists/domain/ShoppingListsInteractorImpl.kt
@@ -0,0 +1,53 @@
+package io.dimasla4ee.shoppinglist.feature.shopping_lists.domain
+
+import io.dimasla4ee.shoppinglist.core.domain.model.ShoppingList
+import io.dimasla4ee.shoppinglist.core.domain.model.ShoppingListIcon
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import org.koin.core.qualifier.named
+
+class ShoppingListsInteractorImpl(
+    private val repository: ShoppingListsRepository
+) : ShoppingListsInteractor {
+
+    override fun getShoppingLists(): Flow<List<ShoppingList>> {
+        return repository.getShoppingLists()
+    }
+
+    override suspend fun addShoppingList(name: String) {
+        val shoppingList = ShoppingList(
+            id = 0,
+            name = name,
+            icon = ShoppingListIcon.SHOPPING_CART,
+            products = emptyList()
+        )
+
+        repository.addShoppingList(shoppingList)
+    }
+
+    override suspend fun deleteShoppingList(shoppingList: ShoppingList) {
+        repository.deleteShoppingList(shoppingList)
+    }
+
+    override suspend fun deleteAllShoppingLists() {
+
+        repository
+            .getShoppingLists()
+            .first()
+            .forEach { repository.deleteShoppingList(it) }
+    }
+
+    override suspend fun updateShoppingList(shoppingList: ShoppingList) {
+        repository.updateShoppingList(shoppingList)
+    }
+
+    override suspend fun duplicateShoppingList(shoppingList: ShoppingList) {
+
+        repository.addShoppingList(
+            shoppingList.copy(
+                id = 0,
+                name = "${shoppingList.name} (копия)"
+            )
+        )
+    }
+}
