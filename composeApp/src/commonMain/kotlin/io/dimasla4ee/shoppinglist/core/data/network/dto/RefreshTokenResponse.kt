74a761c2d5c83c89751bc34ecf276ad2eda058ec
Krylov Dmitry Evgenyevich <94285111+dimasla4ee@users.noreply.github.com>
2026-05-11 08:27:42 +0300

    feature/69-разработка-интерфейса-ktrofit-api-по-документации-swaggerui (#84)
    Resolves #69
    
    - Создал поддиректории `network`:
        - `dto`
        - `api`
    - Переместил классы в соответствующие директории
    - Промаркировал все DTO как ``@Serializable` для сериализации
    - Реализовал интерфейс API
    - Реализовал методы работы с API для эндпоинтов:
        - `POST /auth/registration`
        - `POST /auth/refresh`
        - `POST /auth/recovery`
        - `POST /auth/login`
        - `GET /auth/check`

--- composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/RefreshTokenResponse.kt
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/RefreshTokenResponse.kt
@@ -1,6 +1,9 @@
-package io.dimasla4ee.shoppinglist.core.data.network
+package io.dimasla4ee.shoppinglist.core.data.network.dto
+
+import kotlinx.serialization.Serializable
 
 /** Используется в `/auth/refresh` эндпоинте. */
+@Serializable
 data class RefreshTokenResponse(
     val accessToken: String,
     val refreshToken: String
--- composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/Response.kt
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/Response.kt
@@ -1,10 +1,13 @@
-package io.dimasla4ee.shoppinglist.core.data.network
+package io.dimasla4ee.shoppinglist.core.data.network.dto
+
+import kotlinx.serialization.Serializable
 
 /**
  * Базовый класс сетевых ответов.
  *
  * @param resultCode HTTP-код ответа.
  */
+@Serializable
 open class Response {
     var resultCode = 0
 }
