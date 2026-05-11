cfd219ba5ab01c7910934bb1b03996556d1bc90c
Krylov Dmitry Evgenyevich <94285111+dimasla4ee@users.noreply.github.com>
2026-05-11 08:27:42 +0300

    feature/69-разработка-интерфейса-ktrofit-api-по-документации-swaggerui (#84)
    Resolves #69
    
    - Создал поддиректории `network`:
        - `dto`
        - `api`
    - Переместил классы в соответствующие директории
    - Промаркировал все DTO как ``@Serializable` для сериализации
    - Реализовал интерфейс API
    - Реализовал методы работы с API для эндпоинтов:
        - `POST /auth/registration`
        - `POST /auth/refresh`
        - `POST /auth/recovery`
        - `POST /auth/login`
        - `GET /auth/check`

--- composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/CheckResponse.kt
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/data/network/dto/CheckResponse.kt
@@ -1,6 +1,9 @@
-package io.dimasla4ee.shoppinglist.core.data.network
+package io.dimasla4ee.shoppinglist.core.data.network.dto
+
+import kotlinx.serialization.Serializable
 
 /** Используется в `/auth/check` эндпоинте. */
+@Serializable
 data class CheckResponse(
     val success: Boolean,
     val refresh: Boolean
