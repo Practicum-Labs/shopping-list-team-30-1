40fd79cdefa0d5ca6d5d7de240bcd96823cf5610
Sergei <Nicoanik@hotmail.com>
2026-05-14 20:16:05 +0300

    Delete comma

--- composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/feature/shopping_lists/ui/dialog/CreateListDialog.kt
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/feature/shopping_lists/ui/dialog/CreateListDialog.kt
@@ -78,7 +78,7 @@ fun CreateListDialog(
                     )
                 },
 
-                colors = dialogTextFieldColors(),
+                colors = dialogTextFieldColors()
             )
         },
 
