ca1b552aaffdcd86ca58f0e85a8c353b3841f117
Krylov Dmitry Evgenyevich <94285111+dimasla4ee@users.noreply.github.com>
2026-05-15 13:34:21 +0300

    feature/77-разработка-репозитория-и-юзкейсов-авторизации (#102)
    * feat: внедрить AuthRepository и обработку ошибок на уровне домена
    
    - Добавлены интерфейс `AuthRepository` и его реализация `AuthRepositoryImpl` для обработки логики аутентификации
    - Созданы интерфейсы `DomainResult` и `NetworkError` для обработки ошибок и состояний ответов на доменном уровне
    - Добавлена функция-расширение `toNetworkError()` для преобразования исключений Ktor/Ktorfit в специфичные для домена ошибки
    - Перемещены модели `Response` из пакета DTO данных в пакет доменных моделей
    - Зарегистрирован `AuthRepository` в `DataModule` Koin для внедрения зависимостей
    
    * feat: Создать юзкейсы для каждой функции репозитория
    
    * Feature/lists theme switch (#93)
    
    - добавил возможность переключения темы нажатием на икону в TopBar;
    - сохранение состояние выбранной темы с использованием SharedPreferences;
    - реализована смена иконки при смене темы.
    
    * Fix/81 рефактор welcomescreen (#95)
    
    - Вынес константы размеров в AppDimensions
    - Сверстал экран для альбомного положения
    - Добавил шрифт основного названия в логотипе
    - Переработал создания логотипа для точного центрирования
    - Доработал вёрстку с учётом скалирования в десктопном режиме
    - Добавил провайдера ориентации экрана в utils
    
    * feature/lists-di (#96)
    
    Resolves #87
    
    - Подключение всех зависимостей экрана списков покупок к Koin DI
    
    - Создал временную заглушку для JVM: позже необходимо мигрировать на платформонезависимый Jetpack DataStore
    
    * Add NavigationViewModel (#98)
    
    Создал NavigationViewModel
    
    * chore: Написать документацию по работе над проектом (#79)
    
    - Описал рабочий процесс
    - Указал стиль кода
    
    * feat(di): Добавить юзкейсы в DI
    
    ---------
    
    Co-authored-by: Sviridov-Ivan <sviridov_ivan@mail.ru>
    Co-authored-by: Nicoanik <Nicoanik@hotmail.com>

---
+++ composeApp/src/commonMain/kotlin/io/dimasla4ee/shoppinglist/core/domain/repository/AuthRepository.kt
@@ -0,0 +1,28 @@
+package io.dimasla4ee.shoppinglist.core.domain.repository
+
+import io.dimasla4ee.shoppinglist.core.domain.model.NetworkError
+import io.dimasla4ee.shoppinglist.core.domain.model.Response
+import io.dimasla4ee.shoppinglist.core.domain.model.DomainResult
+
+interface AuthRepository {
+
+    suspend fun register(
+        email: String,
+        password: String
+    ): DomainResult<Response.RegisterResponse, NetworkError>
+
+    suspend fun login(
+        email: String,
+        password: String
+    ): DomainResult<Response.UserAuthResponse, NetworkError>
+
+    suspend fun refresh(
+        refreshToken: String
+    ): DomainResult<Response.RefreshTokenResponse, NetworkError>
+
+    suspend fun recoverPassword(): DomainResult<Response.RecoverPasswordResponse, NetworkError>
+
+    suspend fun checkToken(
+        accessToken: String
+    ): DomainResult<Response.CheckResponse, NetworkError>
+}
